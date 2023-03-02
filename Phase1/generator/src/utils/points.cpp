c8d31b004b4c71f57f0a71ed8e73642780045017
MayorX500 <g0mes.miguel.0120@gmail.com>
2023-03-02 19:25:22 +0000

    Add Generator for Plane, Start Engine

---
+++ Phase1/generator/src/utils/points.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <sstream>
+
+#include "points.h"
+
+Point::Point(double _x, double _y, double _z){
+	x = _x;
+	y = _y;
+	z = _z;
+}
+
+double Point::getX() {
+	return x;
+}
+
+double Point::getY() {
+	return y;
+}
+
+double Point::getZ() {
+	return z;
+}
+
+void Point::setX(double x){
+	this->x = x;
+}
+
+void Point::setY(double y){
+	this->y = y;
+}
+
+void Point::setZ(double z){
+	this->z = z;
+}
+
