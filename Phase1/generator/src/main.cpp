fa6ffbbc5de44ca86966badce1eaa6ea72cea411
MayorX500 <g0mes.miguel.0120@gmail.com>
2023-03-02 19:25:06 +0000

    Add Generator for Plane, Start Engine

--- Phase1/generator/src/main.cpp
+++ Phase1/generator/src/main.cpp
@@ -1,88 +1,69 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<sstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cctype>
+
+#include "points.h"
+#include "models.h"
 
 using namespace std;
 
 static auto const helpMenu =
 "Utilization:\n"
-"\tGenerate a plane: plane [units] [splits] [filename] [normal_axis]\n" //eixo do plano, comprimento do lado do plano, quantos sub_quadrados no plano e nome do ficheiro para guardar
+"\tGenerate a plane: plane [units] [splits] [filename] [normal_axis:{x,y,z}] [colors:{RED,GREEN,BLUE}]\n" //comprimento do lado do plano, quantos sub_quadrados no plano e nome do ficheiro para guardar, Extras: eixo normal do plano, cores?
 "\tGenerate a box: box [side_X] [side_Y] [side_Z] [filename]\n" //comprimento dos lados da caixa e nome do ficheiro
 "\tGenerate a sphere: sphere [radius] [slices] [stacks] [filename]\n" //raio, numero de fatias e numero de camadas e nome do ficheiro
 "\tGenerate a cone: cone [radius] [height] [slices] [stacks] [filename]\n"; //raio, altura, numero de fatias e numero de camadas e nome do ficheiro
 
+void write_to_file(string filename, vector<Point> points) {
+    ofstream file;
+    file.open(filename);
+    file << points.size() << endl;
+    for (auto point : points) {
+	file << point.getX() << "," << point.getY() << "," << point.getZ() << endl;
+    }
+    file.close();
+}
+
+
 int main(int argc, char *argv[]) {
 	if (argc < 4) {
-		std::cout << helpMenu;
+		cout << helpMenu;
 		return 0;
 	}
-	auto todo = std::string(argv[1]);
+	auto todo = string(argv[1]);
 
 	if (todo == "plane") {
 		//plane
-		auto units = std::stof(argv[2]);
-		auto splits = std::stod(argv[3]);
-		auto filename = std::string(argv[4]);
-		auto axis = argc > 5 ? std::string(argv[5]) : "y";
-		std::vector<std::string> vertices;
-		if (axis == "y") {
-			for (int i = 0; i <= splits; i++) {
-				for (int j = 0; j <= splits; j++) {
-					std::stringstream ss;
-					ss << i * units << ",0 ," << j * units << "\n";
-					vertices.push_back(ss.str());
-				}
-			}
-		}
-		else if (axis == "x") {
-			for (int i = 0; i <= splits; i++) {
-				for (int j = 0; j <= splits; j++) {
-					std::stringstream ss;
-					ss << "0, " << i * units << ", " << j * units << "\n";
-					vertices.push_back(ss.str());
-				}
-			}
+		auto units = stod(argv[2]);
+		auto splits = stod(argv[3]);
+		auto filename = string(argv[4]);
+		auto _axis = argc > 5 ? string(argv[5]) : "y";
+		auto axis = (char)tolower(_axis[0]);
 
-		}
-		else if (axis == "z") {
-			for (int i = 0; i <= splits; i++) {
-				for (int j = 0; j <= splits; j++) {
-					std::stringstream ss;
-					ss << i * units << ", " << j * units << ", 0\n";
-					vertices.push_back(ss.str());
-				}
-			}
+		cout << "Generating plane with " << units << " units in size, " << splits << " splits per axis, perpendicular to the" << axis << " axis" << " and saving to " << filename << endl;
 
-		}
-		else {
-			std::cout << "Invalid side\n";
-			return 0;
-		}
+		auto points = draw_plane(units, splits, axis);
+		cout << "Generated " << points.size() << " points" << endl;
 
-		    std::stringstream ss;
-		    for (auto it = vertices.begin(); it != vertices.end(); it++)    {
-			    if (it != vertices.begin()) {}
-			    ss << *it;
-		    }
- 
-		std::cout << "Generating plane...\n" << ss.str() << std::endl;
-
+		write_to_file(filename, points);
 	}
 	else if (todo == "box") {
 		//box
-		std::cout << "box" << std::endl;
+		cout << "box" << endl;
 	}
 	else if (todo == "sphere") {
 		//sphere
-		std::cout << "sphere" << std::endl;
+		cout << "sphere" << endl;
 	}
 	else if (todo == "cone") {
 		//cone
-		std::cout << "cone" << std::endl;
+		cout << "cone" << endl;
 	}
 	else {
-		std::cout << helpMenu;
+		cout << helpMenu;
 	}
 	return 0;
 }
